69931e27e95dc9f464d3b90e17b9ea30677a0c53
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2022-02-23 16:53:28 +0100

    finished setup few bugs to be resolved

--- index.ts
+++ index.ts
@@ -1,372 +1,372 @@
-// #region "importing and cofig stuff"
-import express from "express";
-import Database from "better-sqlite3";
-import cors from "cors";
-import {createApplicant, createInterview, createInterviewer} from "./setup"
+// // #region "importing and cofig stuff"
+// import express from "express";
+// import Database from "better-sqlite3";
+// import cors from "cors";
+// // import {createApplicant, createInterview, createInterviewer} from "./setup"
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
 
-const db = new Database("./data.db", {
-  verbose: console.log,
-});
-// #endregion
+// const db = new Database("./data.db", {
+//   verbose: console.log,
+// });
+// // #endregion
 
-// #region "Sql queries"
-// const getInterviewsFromApplicant = db.prepare(`
-// SELECT interviews.*, interviewers.name as 'interviewerName', interviewers.email as 'interviewerEmail' FROM interviews
-// JOIN interviewers ON interviews.interviewerId = interviewers.id
-// WHERE interviews.applicantId = ?;
+// // #region "Sql queries"
+// // const getInterviewsFromApplicant = db.prepare(`
+// // SELECT interviews.*, interviewers.name as 'interviewerName', interviewers.email as 'interviewerEmail' FROM interviews
+// // JOIN interviewers ON interviews.interviewerId = interviewers.id
+// // WHERE interviews.applicantId = ?;
+// // `)
+
+// const getAllApplicants = db.prepare(`
+// SELECT applicants.* FROM applicants;
+// `);
+
+// const getApplicantById = db.prepare(`
+// SELECT * FROM applicants WHERE id = ?
+// `);
+
+// const getAllInterviewers = db.prepare(`
+// SELECT interviewers.* FROM interviewers;
+// `);
+
+// const getInterviewerById = db.prepare(`
+// SELECT * FROM interviewers WHERE id = ?
+// `);
+
+// const getAllinterviews = db.prepare(`
+// SELECT * FROM interviews;
+// `);
+
+// const getInterviewById = db.prepare(`
+// SELECT * FROM interviews WHERE id = ?
+// `);
+
+// const getApplicantByinterviewerId = db.prepare(`SELECT DISTINCT applicants.*, interviews.date, interviews.score FROM applicants
+// JOIN interviews ON applicants.id = interviews.applicantId
+// WHERE interviews.interviewerId = ?;`)
+
+// const getInterviewerByApplicantId = db.prepare(`SELECT DISTINCT interviewers.*, interviews.date, interviews.score FROM interviewers
+// JOIN interviews ON interviewers.id = interviews.interviewerId
+// WHERE interviews.applicantId = ?;`) // if i want to remove duplicates just remove .date and .score and that new date with same entry will be removed
+
+// const deleteApplicant = db.prepare(`
+// DELETE FROM applicants WHERE id = ?;
 // `)
 
-const getAllApplicants = db.prepare(`
-SELECT applicants.* FROM applicants;
-`);
+// const deleteInterviewer = db.prepare(`
+// DELETE FROM interviewers WHERE id = ?;
+// `)
 
-const getApplicantById = db.prepare(`
-SELECT * FROM applicants WHERE id = ?
-`);
+// const deleteInterview = db.prepare(`
+// DELETE FROM interviews WHERE id = ?;
+// `)
 
-const getAllInterviewers = db.prepare(`
-SELECT interviewers.* FROM interviewers;
-`);
+// // const deleteApplicantInterviews = db.prepare(`
+// // DELETE FROM interviews WHERE applicantId = ?;
+// // `)
 
-const getInterviewerById = db.prepare(`
-SELECT * FROM interviewers WHERE id = ?
-`);
+// // const deleteInterviewerInterviews = db.prepare(`
+// // DELETE FROM interviews WHERE interviewerId = ?;
+// // `)
 
-const getAllinterviews = db.prepare(`
-SELECT * FROM interviews;
-`);
+// const updateApplicant = db.prepare(`
+// UPDATE applicants SET name = ?, email = ?;
+// `)
 
-const getInterviewById = db.prepare(`
-SELECT * FROM interviews WHERE id = ?
-`);
+// const updateInterviewer = db.prepare(`
+// UPDATE interviewers SET name = ?, email = ?;
+// `)
 
-const getApplicantByinterviewerId = db.prepare(`SELECT DISTINCT applicants.*, interviews.date, interviews.score FROM applicants
-JOIN interviews ON applicants.id = interviews.applicantId
-WHERE interviews.interviewerId = ?;`)
+// const updateInterview = db.prepare(`
+// UPDATE interviews SET applicantId = ?, interviewerId = ?, date = ?, score = ?;
+// `)
 
-const getInterviewerByApplicantId = db.prepare(`SELECT DISTINCT interviewers.*, interviews.date, interviews.score FROM interviewers
-JOIN interviews ON interviewers.id = interviews.interviewerId
-WHERE interviews.applicantId = ?;`) // if i want to remove duplicates just remove .date and .score and that new date with same entry will be removed
-
-const deleteApplicant = db.prepare(`
-DELETE FROM applicants WHERE id = ?;
-`)
-
-const deleteInterviewer = db.prepare(`
-DELETE FROM interviewers WHERE id = ?;
-`)
-
-const deleteInterview = db.prepare(`
-DELETE FROM interviews WHERE id = ?;
-`)
-
-// const deleteApplicantInterviews = db.prepare(`
+// const deleteAllInterviewsForApplicant = db.prepare(`
 // DELETE FROM interviews WHERE applicantId = ?;
 // `)
 
-// const deleteInterviewerInterviews = db.prepare(`
+// const deleteAllInterviewsForInterviewer = db.prepare(`
 // DELETE FROM interviews WHERE interviewerId = ?;
 // `)
+// // #endregion
 
-const updateApplicant = db.prepare(`
-UPDATE applicants SET name = ?, email = ?;
-`)
+// // #region 'End points API'
 
-const updateInterviewer = db.prepare(`
-UPDATE interviewers SET name = ?, email = ?;
-`)
+// // #region 'APPLICANTS end points'
+// app.get("/applicants", (req, res) => {
 
-const updateInterview = db.prepare(`
-UPDATE interviews SET applicantId = ?, interviewerId = ?, date = ?, score = ?;
-`)
+//   const applicants = getAllApplicants.all();
 
-const deleteAllInterviewsForApplicant = db.prepare(`
-DELETE FROM interviews WHERE applicantId = ?;
-`)
+//   for (const applicant of applicants) {
 
-const deleteAllInterviewsForInterviewer = db.prepare(`
-DELETE FROM interviews WHERE interviewerId = ?;
-`)
-// #endregion
+//     const interviewer = getInterviewerByApplicantId.all(applicant.id)
+//     applicant.interviewers = interviewer;
 
-// #region 'End points API'
+//   }
 
-// #region 'APPLICANTS end points'
-app.get("/applicants", (req, res) => {
+//   res.send(applicants);
 
-  const applicants = getAllApplicants.all();
+// });
 
-  for (const applicant of applicants) {
+// app.get("/applicants/:id", (req, res) => {
 
-    const interviewer = getInterviewerByApplicantId.all(applicant.id)
-    applicant.interviewers = interviewer;
-
-  }
-
-  res.send(applicants);
-
-});
-
-app.get("/applicants/:id", (req, res) => {
-
-  const id = req.params.id;
-  const applicant = getApplicantById.get(id);
+//   const id = req.params.id;
+//   const applicant = getApplicantById.get(id);
   
-  if (applicant) {
+//   if (applicant) {
 
-    const interviewer = getInterviewerByApplicantId.all(applicant.id)
-    applicant.interviewer = interviewer;
+//     const interviewer = getInterviewerByApplicantId.all(applicant.id)
+//     applicant.interviewer = interviewer;
 
-    res.send(applicant);
+//     res.send(applicant);
 
-  }
+//   }
 
-  else {
-    res.send({"error": "undefined"})
-  }
+//   else {
+//     res.send({"error": "undefined"})
+//   }
 
-});
+// });
 
-app.post('/applicants', (req, res) => {
+// app.post('/applicants', (req, res) => {
 
-  // creating an museum is still the same as last week
-  const { name, email } = req.body
-  const info = createApplicant.run(name, email)
+//   // creating an museum is still the same as last week
+//   const { name, email } = req.body
+//   const info = createApplicant.run(name, email)
 
-  // const errors = []
+//   // const errors = []
 
-  // if (typeof name !== 'string') errors.push()
+//   // if (typeof name !== 'string') errors.push()
 
-  if (info.changes > 0) {
-    const applicant = getApplicantById.get(info.lastInsertRowid)
-    res.send(applicant)
-  } 
+//   if (info.changes > 0) {
+//     const applicant = getApplicantById.get(info.lastInsertRowid)
+//     res.send(applicant)
+//   } 
   
-  else {
-    res.send({ error: 'Something went wrong.' })
-  }
+//   else {
+//     res.send({ error: 'Something went wrong.' })
+//   }
 
-})
+// })
 
-app.delete('/applicants/:id', (req, res) => {
+// app.delete('/applicants/:id', (req, res) => {
 
-  const id = req.params.id
-  deleteAllInterviewsForApplicant.run(id)
-  const info = deleteApplicant.run(id)
+//   const id = req.params.id
+//   deleteAllInterviewsForApplicant.run(id)
+//   const info = deleteApplicant.run(id)
 
-  if (info.changes === 0) {
-    res.status(404).send({ error: 'applicant not found.' })
-  } 
+//   if (info.changes === 0) {
+//     res.status(404).send({ error: 'applicant not found.' })
+//   } 
   
-  else {
-    res.send({ message: 'applicant deleted.' })
-  }
+//   else {
+//     res.send({ message: 'applicant deleted.' })
+//   }
 
-})
+// })
 
-app.patch('/applicants/:id', (req, res) => {
+// app.patch('/applicants/:id', (req, res) => {
 
-  const id = req.params.id;
-  const { name, email } = req.body
+//   const id = req.params.id;
+//   const { name, email } = req.body
 
-  const info = updateApplicant.run(name, email)
-  const updatedApplicant = getApplicantById.get(Number(id))
+//   const info = updateApplicant.run(name, email)
+//   const updatedApplicant = getApplicantById.get(Number(id))
 
-  if (info.changes > 0) {
-    res.send(updatedApplicant)
-  }
+//   if (info.changes > 0) {
+//     res.send(updatedApplicant)
+//   }
 
-  else {
-    res.send({ error: 'Something went wrong.' })
-  }
+//   else {
+//     res.send({ error: 'Something went wrong.' })
+//   }
 
-})
-// #endregion
+// })
+// // #endregion
 
-// #region 'INTERVIEWERS end points'
-app.get("/interviewers", (req, res) => {
+// // #region 'INTERVIEWERS end points'
+// app.get("/interviewers", (req, res) => {
 
-  const interviewers = getAllInterviewers.all();
+//   const interviewers = getAllInterviewers.all();
 
-  for (const interviewer of interviewers) {
+//   for (const interviewer of interviewers) {
 
-    const applicants = getApplicantByinterviewerId.all(interviewer.id)
-    interviewer.applicants = applicants;
+//     const applicants = getApplicantByinterviewerId.all(interviewer.id)
+//     interviewer.applicants = applicants;
 
-  }
+//   }
 
-  res.send(interviewers);
+//   res.send(interviewers);
 
-});
+// });
 
-app.get("/interviewers/:id", (req, res) => {
+// app.get("/interviewers/:id", (req, res) => {
 
-  const id = req.params.id;
-  const interviewer = getInterviewerById.get(id);
+//   const id = req.params.id;
+//   const interviewer = getInterviewerById.get(id);
 
-  if (interviewer) {
+//   if (interviewer) {
 
-    const applicants = getApplicantByinterviewerId.all(interviewer.id)
-    interviewer.applicants = applicants;
+//     const applicants = getApplicantByinterviewerId.all(interviewer.id)
+//     interviewer.applicants = applicants;
 
-    res.send(interviewer);
+//     res.send(interviewer);
 
-  }
+//   }
 
-  else {
-    res.send({"error": "undefined"})
-  }
+//   else {
+//     res.send({"error": "undefined"})
+//   }
 
-});
+// });
 
-app.post('/interviewers', (req, res) => {
+// app.post('/interviewers', (req, res) => {
 
-  const { name, email } = req.body
-  const info = createInterviewer.run(name, email)
+//   const { name, email } = req.body
+//   const info = createInterviewer.run(name, email)
 
-  // const errors = []
+//   // const errors = []
 
-  // if (typeof name !== 'string') errors.push()
+//   // if (typeof name !== 'string') errors.push()
 
-  if (info.changes > 0) {
-    const interviewer = getInterviewerById.get(info.lastInsertRowid)
-    res.send(interviewer)
-  } 
+//   if (info.changes > 0) {
+//     const interviewer = getInterviewerById.get(info.lastInsertRowid)
+//     res.send(interviewer)
+//   } 
   
-  else {
-    res.send({ error: 'Something went wrong.' })
-  }
+//   else {
+//     res.send({ error: 'Something went wrong.' })
+//   }
 
-})
+// })
 
-app.delete('/interviewers/:id', (req, res) => {
+// app.delete('/interviewers/:id', (req, res) => {
 
-  const id = req.params.id
-  deleteAllInterviewsForInterviewer.run(id)
-  const interviews = getAllinterviews.all()
+//   const id = req.params.id
+//   deleteAllInterviewsForInterviewer.run(id)
+//   const interviews = getAllinterviews.all()
 
-  for (const interview of interviews) {
+//   for (const interview of interviews) {
 
-    if (interview.interviewerId === id) {
-      console.log("interview deleting id :", interview)
-      deleteInterview.run(interview.id)
-    }
+//     if (interview.interviewerId === id) {
+//       console.log("interview deleting id :", interview)
+//       deleteInterview.run(interview.id)
+//     }
 
-  }
+//   }
 
-  const info = deleteInterviewer.run(id)
+//   const info = deleteInterviewer.run(id)
 
-  if (info.changes === 0) {
-    res.status(404).send({ error: 'interviewer not found.' })
-  } 
+//   if (info.changes === 0) {
+//     res.status(404).send({ error: 'interviewer not found.' })
+//   } 
   
-  else {
-    res.send({ message: 'interviewer deleted.' })
-  }
+//   else {
+//     res.send({ message: 'interviewer deleted.' })
+//   }
 
-})
+// })
 
-app.patch('/interviewers/:id', (req, res) => {
+// app.patch('/interviewers/:id', (req, res) => {
 
-  const id = req.params.id;
-  const { name, email } = req.body
+//   const id = req.params.id;
+//   const { name, email } = req.body
 
-  const info = updateInterview.run(name, email)
-  const updatedInterviewer = getInterviewerById.get(Number(id))
+//   const info = updateInterview.run(name, email)
+//   const updatedInterviewer = getInterviewerById.get(Number(id))
 
-  if (info.changes > 0) {
-    res.send(updatedInterviewer)
-  }
+//   if (info.changes > 0) {
+//     res.send(updatedInterviewer)
+//   }
 
-  else {
-    res.send({ error: 'Something went wrong.' })
-  }
+//   else {
+//     res.send({ error: 'Something went wrong.' })
+//   }
 
-})
-// #endregion
+// })
+// // #endregion
 
-// #region 'INTERVIEWS end points'
-app.get("/interviews", (req, res) => {
+// // #region 'INTERVIEWS end points'
+// app.get("/interviews", (req, res) => {
 
-  const interviews = getAllinterviews.all();
-  res.send(interviews);
+//   const interviews = getAllinterviews.all();
+//   res.send(interviews);
 
-});
+// });
 
-app.get("/interviews/:id", (req, res) => {
+// app.get("/interviews/:id", (req, res) => {
 
-    const id = req.params.id;
-    const interview = getInterviewById.get(id);
+//     const id = req.params.id;
+//     const interview = getInterviewById.get(id);
 
-    if (interview) {
-      res.send(interview);
-    }
+//     if (interview) {
+//       res.send(interview);
+//     }
 
-    else {
-      res.send({"error": "undefined"})
-    }
+//     else {
+//       res.send({"error": "undefined"})
+//     }
 
-});
+// });
 
-app.post('/interviews', (req, res) => {
+// app.post('/interviews', (req, res) => {
 
-  // creating an museum is still the same as last week
-  const { applicantId, interviewerId, date, score } = req.body
-  const info = createInterview.run(applicantId, interviewerId, date, score)
+//   // creating an museum is still the same as last week
+//   const { applicantId, interviewerId, date, score } = req.body
+//   const info = createInterview.run(applicantId, interviewerId, date, score)
 
-  // const errors = []
+//   // const errors = []
 
-  // if (typeof name !== 'string') errors.push()
+//   // if (typeof name !== 'string') errors.push()
 
-  if (info.changes > 0) {
-    const interview = getInterviewById.get(info.lastInsertRowid)
-    res.send(interview)
-  } 
+//   if (info.changes > 0) {
+//     const interview = getInterviewById.get(info.lastInsertRowid)
+//     res.send(interview)
+//   } 
   
-  else {
-    res.send({ error: 'Something went wrong.' })
-  }
+//   else {
+//     res.send({ error: 'Something went wrong.' })
+//   }
 
-})
+// })
 
-app.delete('/interviews/:id', (req, res) => {
+// app.delete('/interviews/:id', (req, res) => {
 
-  const id = req.params.id
-  const info = deleteInterview.run(id)
+//   const id = req.params.id
+//   const info = deleteInterview.run(id)
 
-  if (info.changes === 0) {
-    res.status(404).send({ error: 'interview not found.' })
-  } 
+//   if (info.changes === 0) {
+//     res.status(404).send({ error: 'interview not found.' })
+//   } 
   
-  else {
-    res.send({ message: 'interview deleted.' })
-  }
+//   else {
+//     res.send({ message: 'interview deleted.' })
+//   }
 
-})
+// })
 
-app.patch('/interviews/:id', (req, res) => {
+// app.patch('/interviews/:id', (req, res) => {
 
-  const id = req.params.id;
-  const { applicantId, interviewerId, date, score } = req.body
+//   const id = req.params.id;
+//   const { applicantId, interviewerId, date, score } = req.body
 
-  const info = updateInterview.run(applicantId, interviewerId, date, score)
-  const updatedInterview = getInterviewById.get(Number(id))
+//   const info = updateInterview.run(applicantId, interviewerId, date, score)
+//   const updatedInterview = getInterviewById.get(Number(id))
 
-  if (info.changes > 0) {
-    res.send(updatedInterview)
-  }
+//   if (info.changes > 0) {
+//     res.send(updatedInterview)
+//   }
 
-  else {
-    res.send({ error: 'Something went wrong.' })
-  }
+//   else {
+//     res.send({ error: 'Something went wrong.' })
+//   }
 
-})
-// #endregion
+// })
+// // #endregion
 
-// #endregion
+// // #endregion
 
-app.listen(4000, () => console.log(`Listening on: http://localhost:4000`));
+// app.listen(4000, () => console.log(`Listening on: http://localhost:4000`));
--- setup.ts
+++ setup.ts
@@ -4,6 +4,7 @@ const db = new Database('./data.db', {
   verbose: console.log
 })
 
+// #region 'Mock data array of objects for db'
 const users = [
   { 
     firstName: 'Jurgen',
@@ -15,7 +16,7 @@ const users = [
     email: "jurgen@email.com" 
   },
   { 
-    name: 'Gozzo',
+    firstName: 'Gozzo',
     lastName: 'Sundberg',
     userName: 'Sundberg28',
     gender: 'M',
@@ -24,7 +25,7 @@ const users = [
     email: "Sundberg@email.com" 
   },
   { 
-    name: 'Iason',
+    firstName: 'Iason',
     lastName: 'Petőfi',
     userName: 'Petőfi25',
     gender: 'M',
@@ -33,13 +34,34 @@ const users = [
     email: "Petőfi@email.com"  
   },
   { 
-    name: 'Ekin',
+    firstName: 'Ekin',
     lastName: 'Machado',
     userName: 'Machado99',
     gender: 'M',
     birthday: '2/12/1994',
     phoneNumber: '0631234533',
     email: "Machado@email.com"  
+  }
+]
+
+const logins = [
+  {
+    status: 'success',
+    dateCreated: '12/11/2021',
+    time: '15:45',
+    userId: 2
+  },
+  {
+    status: 'success',
+    dateCreated: '12/11/2020',
+    time: '17:45',
+    userId: 2
+  },
+  {
+    status: 'success',
+    dateCreated: '3/11/2021',
+    time: '18:45',
+    userId: 1
   }
 ]
 
@@ -53,6 +75,7 @@ const posts = [
     votes: 45,
     createdTime: "21/10/2021",
     userId: 1,
+    subredditId: 2
   },
   { 
     title: "LMAO what a meme",
@@ -63,6 +86,7 @@ const posts = [
     votes: 1257,
     createdTime: "17/10/2021",
     userId: 2,
+    subredditId: 3
   },
   { 
     title: "Khabib beats and submits Conor Mcgregor and reamins undefeated",
@@ -72,7 +96,8 @@ const posts = [
     pic: "image.jpg",
     votes: 5434,
     createdTime: "08/10/2018",
-    userId: 3, 
+    userId: 3,
+    subredditId: 2
   },
   { 
     title: "F1 massive sponsorship is sealed the deal today in italy",
@@ -83,6 +108,7 @@ const posts = [
     votes: 178,
     createdTime: "11/10/2021",
     userId: 4,
+    subredditId: 1
   },
   { 
     title: "A cute cat playing with its owners",
@@ -92,11 +118,12 @@ const posts = [
     pic: "image1.jpg",
     votes: 77,
     createdTime: "1/10/2021",
-    userId: 2,  
+    userId: 2,
+    subredditId: 5  
   }
 ]
 
-const subReddits = [
+const subreddits = [
   {
     name: "formula1",
     followers: 50553,
@@ -123,60 +150,191 @@ const subReddits = [
   }
 ]
 
-const comments = []
+const comments = [
+  {
+    content: "hey yoo",
+    upVotes: 37,
+    downVotes: 11,
+    dateCreated: "21/05/2020",
+    userId: 1,
+    postId: 4
+  },
+  {
+    content: "hey yoo 2",
+    upVotes: 13,
+    downVotes: 18,
+    dateCreated: "21/05/2029",
+    userId: 1,
+    postId: 6
+  },
+  {
+    content: "hey yooooooooo",
+    upVotes: 156,
+    downVotes: 11,
+    dateCreated: "21/05/2022",
+    userId: 2,
+    postId: 4
+  },
+  {
+    content: "coment a",
+    upVotes: 50,
+    downVotes: 144,
+    dateCreated: "11/09/2020",
+    userId: 1,
+    postId: 2
+  },
+  {
+    content: "hey yoooeoehjeohe",
+    upVotes: 2,
+    downVotes: 13,
+    dateCreated: "3/05/2020",
+    userId: 4,
+    postId: 3
+  }
+]
 
+const userSubreddits = [
+  {
+    userId: 2,
+    subredditId: 1
+  },
+  {
+    userId: 1,
+    subredditId: 1
+  },
+  {
+    userId: 3,
+    subredditId: 2
+  },
+  {
+    userId: 1,
+    subredditId: 3
+  }
+]
+// #endregion
+
+// #region 'Creating tables after droping etc sql'
 db.exec(`
-DROP TABLE IF EXISTS interviews;
-DROP TABLE IF EXISTS applicants;
-DROP TABLE IF EXISTS interviewers;
+DROP TABLE IF EXISTS logins;
+DROP TABLE IF EXISTS subreddits;
+DROP TABLE IF EXISTS userSubreddits;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS comments;
 
-CREATE TABLE IF NOT EXISTS applicants (
-  id INTEGER,
-  name TEXT NOT NULL,
-  email TEXT NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS users (
+  "id" INTEGER PRIMARY KEY,
+  "firstName" TEXT NOT NULL,
+  "lastName" INTEGER NOT NULL,
+  "userName" TEXT NOT NULL,
+  "gender" TEXT NOT NULL,
+  "birthday" TEXT NOT NULL,
+  "phoneNumber" TEXT NOT NULL,
+  "email" TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS interviewers (
-  id INTEGER,
-  name TEXT NOT NULL,
-  email TEXT NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS posts (
+  "id" INTEGER PRIMARY KEY,
+  "title" TEXT NOT NULL,
+  "content" TEXT NOT NULL,
+  "linksTo" TEXT NOT NULL,
+  "status" TEXT NOT NULL,
+  "pic" TEXT NOT NULL,
+  "votes" INTEGER NOT NULL,
+  "createdTime" TEXT NOT NULL,
+  "userId" INTEGER NOT NULL,
+  "subredditId" INTEGER NOT NULL,
+  FOREIGN KEY ("userId") REFERENCES "users" ("id")
+  FOREIGN KEY ("subredditId") REFERENCES "subreddits" ("id")
 );
 
-CREATE TABLE IF NOT EXISTS interviews (
-  id INTEGER,
-  applicantId INTEGER,
-  interviewerId INTEGER,
-  date TEXT NOT NULL,
-  score INTEGER NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (applicantId) REFERENCES applicants(id),
-  FOREIGN KEY (interviewerId) REFERENCES interviewers(id)
+CREATE TABLE IF NOT EXISTS subreddits (
+  "id" INTEGER PRIMARY KEY,
+  "name" TEXT NOT NULL,
+  "followers" INTEGER NOT NULL,
+  "online" INTEGER NOT NULL,
+  "dateCreated" TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS comments (
+  "id" INTEGER PRIMARY KEY,
+  "content" TEXT NOT NULL,
+  "upVotes" INTEGER NOT NULL,
+  "downVotes" INTEGER NOT NULL,
+  "dateCreated" TEXT NOT NULL,
+  "userId" INTEGER NOT NULL,
+  "postId" INTEGER NOT NULL,
+  FOREIGN KEY ("userId") REFERENCES "users" ("id"),
+  FOREIGN KEY ("postId") REFERENCES "posts" ("id")
+);
+
+CREATE TABLE IF NOT EXISTS userSubreddits (
+  "id" INTEGER PRIMARY KEY,
+  "userId" INTEGER NOT NULL,
+  "subredditId" INTEGER NOT NULL,
+  FOREIGN KEY ("userId") REFERENCES "users" ("id"),
+  FOREIGN KEY ("subredditId") REFERENCES "subreddits" ("id")
+);
+
+CREATE TABLE IF NOT EXISTS logins (
+  "id" INTEGER PRIMARY KEY,
+  "status" TEXT NOT NULL,
+  "dateCreated" TEXT NOT NULL,
+  "time" TEXT NOT NULL,
+  "userId" INTEGER NOT NULL,
+  FOREIGN KEY ("userId") REFERENCES "users" ("id")
 );
 `)
+// #endregion
 
-export const createApplicant = db.prepare(`
-INSERT INTO applicants (name, email) VALUES (?, ?);
+// #region 'Insert querys'
+export const createUser = db.prepare(`
+INSERT INTO users ( firstName, lastName, userName, gender, birthday, phoneNumber, email ) VALUES (?, ?, ?, ?, ?, ?, ?);
 `)
 
-export const createInterviewer = db.prepare(`
-INSERT INTO interviewers (name, email) VALUES (?, ?);
+export const createComment = db.prepare(`
+INSERT INTO comments (content, upVotes, downVotes, dateCreated, userId, postId) VALUES (?, ?, ?, ?, ?, ?);
 `)
 
-export const createInterview = db.prepare(`
-INSERT INTO interviews (applicantId, interviewerId, date, score)
-VALUES (?, ? ,?, ?);
+export const createUserSubreddit = db.prepare(`
+INSERT INTO userSubreddits (userId, subredditId) VALUES (?, ?);
 `)
 
-for (const applicant of applicants) {
-  createApplicant.run(applicant.name, applicant.email)
+export const createPost = db.prepare(`
+INSERT INTO posts (title, content, linksTo, status, pic, votes, createdTime, userId, subredditId ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+`)
+
+export const createLogin = db.prepare(`
+INSERT INTO logins (status, dateCreated, time, userId) VALUES (?, ?, ?, ?);
+`)
+
+export const createSubreddit = db.prepare(`
+INSERT INTO subreddits (name, followers, online, dateCreated ) VALUES (?, ? ,?, ?);
+`)
+// #endregion
+
+// #region 'For of loops to insert from mockdata to db with running the sql query'
+for (const user of users) {
+  createUser.run(user.firstName, user.lastName, user.userName, user.gender, user.birthday, user.phoneNumber, user.email)
 }
 
-for (const interviewer of interviewers) {
-  createInterviewer.run(interviewer.name, interviewer.email)
+for (const login of logins) {
+  createLogin.run(login.status, login.dateCreated, login.time, login.userId)
 }
 
-for (const interview of interviews) {
-  createInterview.run(interview.applicantId, interview.interviewerId, interview.date, interview.score)
+for (const post of posts) {
+  createPost.run(post.title, post.content, post.linksTo, post.status, post.pic, post.votes, post.createdTime, post.userId, post.subredditId)
 }
+
+for (const subreddit of subreddits) {
+  createSubreddit.run(subreddit.name, subreddit.followers, subreddit.online, subreddit.dateCreated)
+}
+
+for (const userSubreddit of userSubreddits) {
+  createUserSubreddit.run(userSubreddit.userId, userSubreddit.subredditId)
+}
+
+for (const comment of comments) {
+  createComment.run(comment.content, comment.upVotes, comment.downVotes, comment.dateCreated, comment.userId, comment.postId)
+}
+// #endregion
